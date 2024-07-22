3c68bf2947f6611217d699b33d1905fcce78cc43
benjamin <wuqiong3699@gmail.com>
2024-07-22 22:52:28 +0800

    optimized 3

---
+++ components/BgCircle.tsx
@@ -0,0 +1,9 @@
+export function BgCirle(){
+  return (
+    <>
+      <div>
+        
+      </div>
+    </>
+  )
+}
--- components/homePage/personalInfo.tsx
+++
@@ -1,7 +0,0 @@
-export function PersonInfo(){
-  return (
-    <>
-      personal information
-    </>
-  )
-}
