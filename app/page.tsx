2ac74f076a0411a2d74664bee114eff25c0efc55
benjamin <wuqiong3699@gmail.com>
2024-07-23 16:50:10 +0800

    单独封装零散的css

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,11 @@
 export default function Home() {
   return (
     <>
-      {/* <section className="h-64 mb-10 border border-red-500">
+     <section className="h-64 mb-10 mt-10 border border-red-500">
         <div>
           personal infomation
         </div>
-      </section>
-      <section className="h-64 mb-10 border border-red-500">
-        <div>
-          blogs infomation
-        </div>
-      </section>
-      <section className="h-64 mb-10 border border-red-500">
-        <div>
-          codes infomation
-        </div>
-      </section>
-      <section className="h-64 mb-10 border border-red-500">
-        <div>
-          skills infomation
-        </div>
-      </section> */}
+      </ section>
     </>
   );
 }
