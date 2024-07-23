17d9fac305aa5864b0c7c9624bc3746cabd0daa1
benjamin <wuqiong3699@gmail.com>
2024-07-23 18:12:09 +0800

    personal information module is builded

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
 export default function Home() {
   return (
     <>
-     <section className="h-64 mb-10 mt-10 border border-red-500">
-        <div>
-          personal infomation
+      <section className="p-10 mb-10 mt-10">
+        <div className="flex items-center justify-center">
+          <div className="p-10 rounded-3xl gray-gradient">
+            <p className="text-xl font-bold">My name is:</p>
+            <p className="p-5 text-3xl font-extrabold">Benjamin59</p>
+            <hr className="pt-5 border-t-3" />
+            <p className="w-80 text-xl font-bold">I'm a:</p>
+            <div className="my-2 flex flex-col items-end">
+              <p className="text-xl py-0.5">Full Stack Developer</p>
+              <p className="text-xl py-0.5">Reactjs Developer</p>
+              <p className="text-xl py-0.5">Nestjs Developer</p>
+              <p className="text-xl py-0.5">Fitness fan</p>
+              <p className="text-xl py-0.5">LOL fan</p>
+            </div>
+          </div>
         </div>
       </ section>
     </>
