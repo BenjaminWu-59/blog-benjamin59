e576345e8b9ea787b9e1d346e9a3af52f2ce4a33
benjamin <wuqiong3699@gmail.com>
2024-08-09 17:55:47 +0800

    absolute position centered

--- components/PersonalAnimate.tsx
+++ components/PersonalAnimate.tsx
@@ -4,14 +4,14 @@ const PersonalAnimate = () => {
   return (
     <>
       <div className="w-full h-[550px] absolute overflow-hidden">
-        <div className="w-[700px] h-[700px] absolute inset-0 m-auto flex justify-center items-center">
+        <div className="w-[700px] h-[700px] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
           <div className="real w-full h-full flex justify-center items-center
                rounded-full border-dashed border-2"
             style={{ borderColor: "rgba(209, 213, 219, 0.4)" }}>
           </div>
         </div>
 
-        <div className="w-[600px] h-[600px] absolute inset-0 m-auto flex justify-center items-center">
+        <div className="w-[600px] h-[600px] absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
           <div className="real w-full h-full flex justify-center items-center 
                rounded-full border-dashed border-2"
             style={{ borderColor: "rgba(209, 213, 219, 0.8)" }} />
