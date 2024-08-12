be00e11fd24231deefe9ed515030a35b5491f33e
benjamin <wuqiong3699@gmail.com>
2024-08-12 14:34:39 +0800

    sections spacing optimiazed

--- app/page.tsx
+++ app/page.tsx
@@ -21,12 +21,12 @@ const Home: React.FC = () => {
 
   return (
     <>
-      <section className="z-10 w-full px-4 py-[200px] mt-[50px] relative flex flex-wrap-reverse justify-items-center justify-around items-center">
+      <section className="z-10 w-full px-4 py-[220px] mt-[50px] relative flex flex-wrap-reverse justify-items-center justify-around items-center">
         <PersonalAnimate />
         <PersonalCard />
       </ section>
 
-      <section id="blogs" className="px-4 lg:px-16 xl:px-32 2xl:px-44 py-32">
+      <section id="blogs" className="px-4 lg:px-16 xl:px-32 2xl:px-44 pt-[20px] pb-[200px]">
         <p className="relative inline-block text-2xl font-medium pb-1 px-1 rounded-lg bg-gradient-to-r from-orange-300 to-orange-300 text-black dark:text-white">
           Here are some of the articles I wrote, including technical articles, essays, etc.
         </p>
@@ -35,21 +35,21 @@ const Home: React.FC = () => {
 
       <section id="skills" 
                ref={skillsRef} 
-               className={`px-4 lg:px-16 xl:px-32 2xl:px-44 py-44
+               className={`px-4 lg:px-16 xl:px-32 2xl:px-44 py-32
                ${isSkillVisible ? 'zoom-in' : ''}`}>
         <SkillList skill={testSkillList} />
       </section>
 
       <section id="projects" 
                ref={projectsRef} 
-               className={`px-4 lg:px-16 xl:px-32 2xl:px-44 py-44 
+               className={`px-4 lg:px-16 xl:px-32 2xl:px-44 py-32
                 ${isProjectVisible ? 'zoom-in' : ''}`}>
         <ProjectList project={testProjectList} />
       </section>
 
       <section id="fitness" 
                ref={fitnessRef} 
-               className={`px-4 lg:px-16 xl:px-32 2xl:px-44 py-28 
+               className={`px-4 lg:px-16 xl:px-32 2xl:px-44 py-32 
                 ${isFitnessVisible ? 'zoom-in' : ''}`}>
         <FitnessList
           diets={testDietList}
