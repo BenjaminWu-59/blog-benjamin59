bef2fe9485035ad09cd657a5aee6aaa01100a3a6
benjamin <wuqiong3699@gmail.com>
2024-08-12 17:49:49 +0800

    git config core.ignorecase false to fixed the error by file name change

---
+++ components/MainNav.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { siteConfig } from "@/config/site";
+import { Icons } from "./Icons";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export function MainNav() {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center space-x-4 lg:space-x-6 justify-center">
+      <Link href="/" className="mr-2 flex items-center justify-center space-x-3">
+        <Icons.logo className="h-8 w-8 rounded-3xl" />
+        <span className="text-3xl font-zain font-bold text-foreground/80">{siteConfig.name}</span>
+      </Link>
+      <Link
+        href="#blogs"
+        className={cn(
+          "text-lg font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Blogs
+      </Link>
+      <Link
+        href="#skills"
+        className={cn(
+          "text-lg font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/about" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Skills
+      </Link>
+      <Link
+        href="#projects"
+        className={cn(
+          "text-lg font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/about" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Projects
+      </Link>
+      <Link
+        href="#fitness"
+        className={cn(
+          "text-lg font-medium transition-colors hover:text-primary hidden sm:inline-block",
+          pathname === "/about" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Fitness
+      </Link>
+    </nav>
+  );
+}
