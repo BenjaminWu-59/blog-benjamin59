08c55302233f513170ced94a3afab6bb4a89a779
benjamin <wuqiong3699@gmail.com>
2024-08-14 15:00:07 +0800

    light and dark mode completed

--- components/pages/BgAnimate.tsx
+++
@@ -1,9 +0,0 @@
-export function BgforCircle(){
-  return (
-    <>
-      <div className="w-full h-full absolute z-0 flex justify-center items-center overflow-hidden border border-red-500">
-        
-      </div>
-    </>
-  )
-} 
