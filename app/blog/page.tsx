07cc7e6edd49aeb5ca8178344036a707fb0ed7ed
benjamin <wuqiong3699@gmail.com>
2024-08-16 15:29:02 +0800

    velite mdx config

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,18 @@
 
-const BlogPage = () =>{
-   return (
-     <>
-       <div>Blog Page</div>
-     </>
-   )
+
+const BlogPage = () => {
+  return (
+    <div className="container max-w-4xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
+          <p className="text-xl text-muted-foreground">
+            My ramblings on all things web dev.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default BlogPage
